5befbd61690cbc310ad1c0484b1faac252d41496
Carrotman42 <kevin@chowski.com>
2013-02-18 17:02:43 -0500

    Bugfix
    I should really get some unit tests up

--- java/src/com/ksoft/btx/BTXFile.java
+++ java/src/com/ksoft/btx/BTXFile.java
@@ -17,6 +17,10 @@ public class BTXFile {
 	public static BTXObject[] readIntoMemory(File src) throws IOException {
 		try (DataInputStream f = new DataInputStream(new BufferedInputStream(new FileInputStream(
 				src)))) {
+			int v = f.readByte();
+			if (v != 0) {
+				throw new UnsupportedOperationException("Unsupported BTX version: " + v);
+			}
 			int rootCount = f.readInt();
 			BTXObject[] ret = new BTXObject[rootCount];
 			for (int i = 0; i < rootCount; i++) {
