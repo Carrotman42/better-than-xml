484b1a2aca6f568a69975fa6183ba102bc25601c
Carrotman42 <kevin@chowski.com>
2013-02-19 17:32:07 -0500

    Test actually tests now
    I'll make it more of an in depth test soon

--- java/src/com/ksoft/btx/test/TestBTXPusherParser.java
+++ java/src/com/ksoft/btx/test/TestBTXPusherParser.java
@@ -1,11 +1,17 @@
 package com.ksoft.btx.test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.Test;
 
-import com.ksoft.btx.BTXEvent;
+import com.ksoft.btx.BTXAttribute;
 import com.ksoft.btx.BTXParser;
 import com.ksoft.btx.BTXPusher;
 
@@ -31,28 +37,60 @@ public class TestBTXPusherParser {
 	}
 	
 	@Test
-	public void t() throws IOException {
-		try (BTXParser parse = new BTXParser(testFile)) {
-			BTXEvent ev;
-			String indent = "";
-			while ((ev = parse.next()) != BTXEvent.EOF) {
-				switch (ev) {
+	public void doTheTest() throws IOException {
+		File dest = File.createTempFile("asdfff", "");
+		try (BTXParser parse = new BTXParser(testFile); BTXPusher push = new BTXPusher(dest)) {
+			l:
+			while (true) {
+				switch (parse.next()) {
 					case ATTRIBUTE :
-						System.out.println(indent
-											+ "Attribute: " + parse.getEventData().getAttribute());
+						BTXAttribute attr = parse.getEventData().getAttribute();
+						byte[] buf = new byte[attr.getLength()];
+						attr.fill(buf);
+						push.addAttribute(attr.getName(), buf, buf.length);
 						break;
 					case START_OBJECT :
-						indent += "\t";
-						System.out.println(indent + "Object: " + parse.getEventData().objName);
+						push.startObject(parse.getEventData().objName);
 						break;
 					case END_OBJECT :
-						indent = indent.substring(1);
+						push.endObject();
 						break;
-					default :
-						System.out.println(indent + ev);
-						break;
+					case EOF :
+						break l;
 				}
 			}
 		}
+		
+		compareFiles(testFile, dest);
+	}
+	
+	public static void compareFiles(File f1, File f2) throws IOException {
+		assertEquals(f1.length(), f2.length());
+		try (BufferedInputStream b1 = new BufferedInputStream(new FileInputStream(f1));
+				BufferedInputStream b2 = new BufferedInputStream(new FileInputStream(f2))) {
+			byte[] buf1 = new byte[1024 * 16], buf2 = new byte[1024 * 16];
+			
+			while (true) {
+				boolean eof = readFully(b1, buf1);
+				if (eof) {
+					break;
+				}
+				readFully(b2, buf2);
+				assertArrayEquals(buf1, buf2);
+			}
+		}
+	}
+	
+	private static boolean readFully(InputStream in, byte[] buf) throws IOException {
+		int len = buf.length;
+		int pos = 0;
+		while (pos < len) {
+			int read = in.read(buf, pos, len - pos);
+			if (read == -1) {
+				return true;
+			}
+			pos += read;
+		}
+		return false;
 	}
 }
